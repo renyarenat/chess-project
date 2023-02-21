02b5e1f4dbaaacecf5a5475b218d07b3ac68ac2f
Renat <renyamazunin@gmail.com>
2023-02-21 22:08:33 +0300

    moving logic in process

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -33,7 +33,7 @@ export class Cell {
     const min = Math.min(this.y, target.y)
     const max = Math.max(this.y, target.y)
     for (let y = min + 1; y < max; y++) {
-      if (this.board.getCell(this.x, y).isEmpty()) {
+      if (!this.board.getCell(this.x, y).isEmpty()) {
         return false
       }
     }
@@ -41,17 +41,47 @@ export class Cell {
   }
 
   isEmptyHorizontal (target: Cell): boolean {
+    if (this.y !== target.y) {
+      return false
+    }
+
+    const min = Math.min(this.x, target.x)
+    const max = Math.max(this.x, target.x)
+    for (let x = min + 1; x < max; x++) {
+      if (!this.board.getCell(x, this.y).isEmpty()) {
+        return false
+      }
+    }
     return true
   }
 
   isEmptyDiagonal (target: Cell): boolean {
+    const absX = Math.abs(target.x - this.x)
+    const absY = Math.abs(target.y - this.y)
+    if (absX !== absY) {
+      return false
+    }
+    const dy = this.y < target.y ? 1 : -1
+    const dx = this.x < target.x ? 1 : -1
+
+    for (let i = 1; i < absY; i++) {
+      if (!this.board.getCell(this.x + dx * i, this.y + dy * i).isEmpty()) {
+        return false
+      }
+    }
     return true
+  }
+
+  setFigure (figure: Figure | null): void {
+    this.figure = figure
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.figure!.cell = this
   }
 
   moveFigure (target: Cell): void {
     if ((this.figure?.canMove(target)) ?? false) {
       this.figure?.moveFigure(target)
-      target.figure = this.figure
+      target.setFigure(this.figure)
       this.figure = null
     }
   }
